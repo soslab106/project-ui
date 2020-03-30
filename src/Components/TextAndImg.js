a690c78c115498575cfae98cc8a0faa9c84bd0dc
pauleezz <106306033@nccu.edu.tw>
2020-03-30 22:44:24 +0800

    init nnintro container and component

---
+++ src/Components/TextAndImg.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TextAndImg extends Component {
+    render() {
+        return (
+            <div className='d-flex'>
+                <div className={'col-'+this.props.textWidth}>{this.props.text}</div>
+                <div className={'col-'+this.props.imgWidth}><img src={this.props.imgPath}/></div>
+            </div>
+        );
+    }
+}
+
+export default TextAndImg;
--- src/Container/NeuralNetworkIntro.js
+++ src/Container/NeuralNetworkIntro.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from './node_modules/react';
+import TextAndImg from '../Component/TextAndImg';
 
 class NeuralNetworkIntro extends Component {
     render() {
         return (
             <div>
-                
+                <h1>類神經網路</h1>
+                <h2>Artificial Neural Network</h2>
+                <TextAndImg
+                    textWidth='9'
+                    imgWidth='3'
+                    text='gijhfiosajriergtre'
+                    imgPath='../../public/logo192.png'
+                />
             </div>
         );
     }
