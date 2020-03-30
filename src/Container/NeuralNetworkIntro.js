8b82b0a0dcad43f567e369758cdccace1501ae6b
pauleezz <106306033@nccu.edu.tw>
2020-03-30 22:29:50 +0800

    please success

---
+++ src/Container/NeuralNetworkIntro.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NeuralNetworkIntro extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default NeuralNetworkIntro;
