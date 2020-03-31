50ced927500215d8536781d502cdf4b0caf80010
Joanna Chien <joannachou713@gmail.com>
2020-03-31 19:59:53 +0800

    add content in nn-intro

--- src/Container/NeuralNetworkIntro.js
+++ src/Container/NeuralNetworkIntro.js
@@ -2,6 +2,10 @@ import React, { Component } from '../../node_modules/react';
 import TextAndImg from '../Components/TextAndImg';
 
 class NeuralNetworkIntro extends Component {
+    // this.state = {
+    //     content:['深度學習是機器學習的一個分支，二者均為人工智慧領域的重要理論。其中，類神經網路是現今「深度學習」的主角之一。所謂的類神經網路就是模仿大腦運作的機器學習方式。',
+    //             '人腦內的神經網路，主要是由神經元與突觸所組成。而神經網路是由一層層的神經元連接而成，每個階層之間的神經元都有著不同強度的鍵結，傳送資訊，進而完成思考判斷的功能，而類神經網路就是參照這樣的概念而誕生的。']
+    // }
     render() {
         return (
             <div className='container'>
