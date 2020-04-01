de73df1ac250ace8e39a5e738223f9aec2d82b29
Joanna Chien <joannachou713@gmail.com>
2020-04-01 12:25:32 +0800

    add route

--- src/index.js
+++ src/index.js
@@ -5,3 +5,25 @@ import App from './Components/App'
 
 
 ReactDOM.render(<App />, document.querySelector('#root'))
+
+/////
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { routes } from './component/routes';
+import { renderRoutes } from 'react-router-config'
+import './index.css';
+import Navbar from './component/Frame/Navbar'
+
+//login = localstorage sess or cookie
+ReactDOM.render((
+    <BrowserRouter>
+        <Navbar login={true} />
+        <div className='d-flex'>
+            {renderRoutes(routes)}
+        </div>
+    </BrowserRouter>
+), document.getElementById('root'));
+
+
+// ReactDOM.render(<Main />, document.getElementById('root'));
---
+++ src/route.js
@@ -0,0 +1,9 @@
+import NeuralNetworkIntro from './Container/NeuralNetworkIntro'
+
+export const routes = [
+    {
+        path: '/nn-intro',
+        component: NeuralNetworkIntro,
+        exact: true
+    },
+]
