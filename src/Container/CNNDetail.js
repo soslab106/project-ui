855c39715ff12bcdca30f0d291ae72bd189ab7de
pauleezz <106306033@nccu.edu.tw>
2020-05-01 17:38:24 +0800

    cnn-detail

---
+++ src/Container/CNNDetail.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CNNDetail extends Component {
+    render() {
+        return (
+            <div className="container d-flex align-items-center">
+                
+            </div>
+        );
+    }
+}
+
+export default CNNDetail;
