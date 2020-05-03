441f4a0df48a786e298bdcee042031a2ee21200c
pauleezz <106306033@nccu.edu.tw>
2020-05-03 19:25:59 +0800

    wejiqjqwoe

--- src/Container/PlayGroundList.js
+++ src/Container/PlayGroundList.js
@@ -5,9 +5,10 @@ class PlayGroundList extends Component {
     render() {
         return (
             <div className="container expand-full d-flex flex-column ">          
-                <h2 className="title mb-3">操作模型選擇</h2>
                 <div className="d-flex">
-                    <div className="col-3">
+                    <div className="col-4">
+                <h2 className="title mb-3 text-center p-3">操作模型選擇</h2>
+
                         <FullWidthBlock py="4" my="0">
                             <div className="d-flex flex-column align-items-center">
                                 <div className="my-4">
@@ -21,7 +22,7 @@ class PlayGroundList extends Component {
                                     <h3 className="title">物體定位</h3>
                                     <div className="d-flex flex-column">
                                         <a href="/yolov3-playground"><div className="col-1">Yolov3</div></a>
-                                    </div>                                
+                                    </div>
                                 </div>
                                 <div className="my-4">
                                     <h3 className="title">人臉辨識</h3>
@@ -32,8 +33,8 @@ class PlayGroundList extends Component {
                             </div>
                         </FullWidthBlock>
                     </div>
-            <div className="col-9">
-                <img src="/images/intro_1.png"/>
+            <div className="col-8 p-5 ml-5">
+                <img height="500" src="/images/model-select.png"/>
             </div> 
                 </div>
             
