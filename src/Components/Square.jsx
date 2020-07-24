f27ab96f17af2d561f94a32c911d8a7539489644
pauleezz <106306033@nccu.edu.tw>
2020-07-24 19:44:19 +0800

    0724

--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Square extends Component {
-    render() {
-        return (
-
-            <a href={this.props.link}>
-                <div style={{ width: "200px", height: "200px" }}>{this.props.buttonText}</div>
-            </a>
-
-        );
-    }
+  render() {
+    return (
+      <a href={this.props.link}>
+        <div
+          className="d-flex align-items-center justify-content-center border border-warning bg-white m-5"
+          style={{ width: "200px", height: "200px" }}
+        >
+          <div className="text-success text-center">{this.props.buttonText}</div>
+        </div>
+      </a>
+    );
+  }
 }
 
 export default Square;
--- src/Container/Menu.jsx
+++ src/Container/Menu.jsx
@@ -4,7 +4,8 @@ import Square from '../Components/Square';
 class Menu extends Component {
     render() {
         return (
-            <div>
+            <div class="container d-flex flex-column align-items-center p-3 mb-2">
+                <div class="d-flex">
                 <Square
                     link=''
                     buttonText='Testing'
@@ -13,15 +14,17 @@ class Menu extends Component {
                     link=''
                     buttonText='TL'
                 />
+                </div>
+                <div className='d-flex'>
                 <Square
                     link=''
                     buttonText='AIInfo'
                 />
-                <Square
+                <Square 
                     link=''
                     buttonText='個人資料'
                 />
-
+                </div>
             </div>
         );
     }
