9b1fc0f8e728e578d9af798691e72cce3137151e
Joanna Chien <joannachou713@gmail.com>
2020-11-06 23:57:00 +0800

    model-list and custom fix result

--- src/Container/CustomModel/PersonalModelView.jsx
+++ src/Container/CustomModel/PersonalModelView.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 class PersonalModelView extends Component {
   // Call API to get users' all custom models
@@ -13,53 +13,55 @@ class PersonalModelView extends Component {
         modelName: "aaa",
         date: "2020/09/10",
         base: "VGG",
-        modellink: `${localStorage.getItem("username")} abc`,
       },
       {
         modelName: "bbb",
         date: "2020/09/11",
         base: "VGG",
-        modellink: `${localStorage.getItem("username")} abc`,
       },
       {
         modelName: "ccc",
         date: "2020/09/12",
         base: "VGG",
-        modellink: `${localStorage.getItem("username")} abc`,
       },
       {
         modelName: "ddd",
         date: "2020/09/13",
         base: "VGG",
-        modellink: `${localStorage.getItem("username")} abc`,
       },
       {
         modelName: "ddd",
         date: "2020/09/13",
         base: "VGG",
-        modellink: `${localStorage.getItem("username")} abc`,
-      }
+      },
     ],
   };
 
-  componentDidMount = ()=>{
-    axios.get('http://140.119.19.99:8000/upload/load/', {
-      headers: {
-        Authorization: `JWT ${localStorage.getItem("token")}`,
-      }})
-      .then(res=>{
-        const result = res.data.result
-        console.log(result)
-        this.setState({modelList:result})
+  componentDidMount = () => {
+    if (!localStorage.getItem("token")) {
+      window.location.href = "/signin";
+      alert("請先登入再使用本功能!");
+    }
+
+    axios
+      .get("http://140.119.19.99:8000/upload/load/", {
+        headers: {
+          Authorization: `JWT ${localStorage.getItem("token")}`,
+        },
       })
-      .catch(error=>{
-        if(!error.response){
-          console.log('network error')
-        }else{
-          console.log(error.response.data.message)
+      .then((res) => {
+        const result = res.data.result;
+        console.log(result);
+        this.setState({ modelList: result });
+      })
+      .catch((error) => {
+        if (!error.response) {
+          console.log("network error");
+        } else {
+          console.log(error.response.data.message);
         }
-      })
-  }
+      });
+  };
 
   renderTable = () => {
     return this.state.modelList.map((model) => (
@@ -68,7 +70,7 @@ class PersonalModelView extends Component {
           <div>{model.name}</div>
           <div>{model.date}</div>
           <div>VGG16</div>
-          <div>{model.name}</div>
+          <Link to={{pathname: '/execution'}}><div><button className='btn btn-main'>前往使用</button></div></Link>
         </div>
       </>
     ));
@@ -82,8 +84,12 @@ class PersonalModelView extends Component {
       >
         <div className="d-flex justify-content-center align-items-center">
           <div className="secondary-title">個人模型庫</div>
-          <div className='ml-3' id='model-list-add'>
-          <Link to={{pathname:'/new-model'}}><div className="d-flex justify-content-center align-items-center h-100"><FontAwesomeIcon icon={faPlus} style={{color:'#F6F8F8'}}/></div></Link>
+          <div className="ml-3" id="model-list-add">
+            <Link to={{ pathname: "/new-model" }}>
+              <div className="d-flex justify-content-center align-items-center h-100">
+                <FontAwesomeIcon icon={faPlus} style={{ color: "#F6F8F8" }} />
+              </div>
+            </Link>
           </div>
         </div>
 
